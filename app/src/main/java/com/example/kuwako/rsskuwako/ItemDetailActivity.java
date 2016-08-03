0710e025dddd0f6e78f2c87e452166778a1ea24b
kuwako <kuwako@leverages.jp>
2016-08-03 20:52:27 +0900

    一通りサンプル完成

---
+++ app/src/main/java/com/example/kuwako/rsskuwako/ItemDetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.kuwako.rsskuwako;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+            private TextView mTitle;
+            private TextView mBody;
+
+            @Override
+            public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_item_detail);
+
+                Intent intent = getIntent();
+
+                String title = intent.getStringExtra("title");
+                mTitle = (TextView) findViewById(R.id.item_detail_title);
+                mTitle.setText(title);
+                String body = intent.getStringExtra("body");
+                mBody = (TextView) findViewById(R.id.item_detail_body);
+                mBody.setText(body);
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/kuwako/rsskuwako/MainActivity.java
+++ app/src/main/java/com/example/kuwako/rsskuwako/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kuwako.rsskuwako;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity
     public static final String RSS_FEED_URL = "http://itpro.nikkeibp.co.jp/rss/ITpro.rdf";
     private ArrayList<Item> mItems;
     private RssListAdapter mAdapter;
+    public static final int MENU_ITEM_RELOAD = Menu.FIRST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +31,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -55,6 +58,15 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Item item = mItems.get(position);
+        Intent intent = new Intent(this, ItemDetailActivity.class);
+        intent.putExtra("title", item.getTitle());
+        intent.putExtra("body", item.getBody());
+        startActivity(intent);
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -66,19 +78,16 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        boolean result = super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        menu.add(0, MENU_ITEM_RELOAD, 0, "更新")
+        return result;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
